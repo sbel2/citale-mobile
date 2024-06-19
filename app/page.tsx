383705db049ee1480da88f2d6cc8ff1da1027649
Sid <sidharth.singh0412@gmail.com>
2024-06-19 09:59:28 -0700

    Dynamic route to Event

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,39 @@
 import Card from "@/components/card";
+//import { createClient } from "@/supabase/client";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   const events = [
     {
       id: 0,
-      name: "Where to go this weekend | come to Boston Seaport Summer Market",
-      description:
-        "The Seaport Summer Market will be returning for the third year! You can visit from 05/11 until 09/22 on the weekends from 11 am to 7 pm 🤠 Enjoy artisan grilled cheese sandwiches 🥪 from Cheese Louise, empanadas from Bocadillos, cuisine from the Caribbeans at ZaZ, and hard cider 🍷 from Downeast. You can also support local vendors at the Makers Show. From vintage clothing and jewelry to caucuses 🌵, browse through the 80 stalls at the market to find your next obsession!📍88 Seaport Boulevard, right next to Warby Parker 📅 May 11th - Sep 22nd 🚇 take the subway to State stop, and walk for 15-20 minutes",
+      title:
+        "Where to go this weekend | come to Boston Seaport Summer Market ☘️",
+      description: `The Seaport Summer Market will be returning for the third year! You can visit from 05/11 until 09/22 on the weekends from 11 am to 7 pm 🤠
+
+      Enjoy artisan grilled cheese sandwiches 🥪 from Cheese Louise, empanadas from Bocadillos, cuisine from the Caribbeans at ZaZ, and hard cider 🍷 from Downeast.
+      
+      You can also support local vendors at the Makers Show. From vintage clothing and jewelry to caucuses 🌵, browse through the 80 stalls at the market to find your next obsession!
+      
+      📍88 Seaport Boulevard, right next to Warby Parker
+      
+      📅 May 11th - Sep 22nd
+      
+      🚇 take the subway to State stop, and walk for 15-20 minutes
+      
+      🔗 https://www.bostonseaport.xyz/venue/seaport-summer-market-2/`,
+
       imageUrl:
-        "https://prod-files-secure.s3.us-west-2.amazonaws.com/6816f889-5e57-485b-ab46-d40ad54d54f4/a314acf3-c5eb-4beb-b8f8-77d301d6138e/seaport1.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20240618%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20240618T050003Z&X-Amz-Expires=3600&X-Amz-Signature=45f38f0ecbd437298e6480f649ca5e3cf7f7ef2cb6a69db74bb460c38dad4e9e&X-Amz-SignedHeaders=host&x-id=GetObject",
+        "https://qteefmlwxyvxjvehgjvp.supabase.co/storage/v1/object/public/images/e1_1.jpeg",
     },
-  ];
+  ]; // Dummy data. Have to replace with the backend data
+  //const supabase = createClient();
+  //const { data: events, error } = await supabase.from("cueup-events").select();
+
+  //console.log(events); // TODO : Make sure after fetching the data, the events are logged
+  //console.log(error);
+  // if (!events) {
+  //   return <p>Not Found </p>;
+  // }
   return (
     <main className="min-h-screen mx-auto max-w-[100rem]">
       <div className="px-12 pt-12 pb-20">
@@ -22,13 +44,21 @@ export default function Home() {
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12"></div>
           {events.map((event) => (
-            <Card key={`${event.name} - ${event.id}`} {...event} />
+            <Card
+              key={event.id}
+              {...event}
+              //imageUrl={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${event.imageUrl}`}
+            />
           ))}
         </div>
         <h2 className="text-4xl mt-20 mb-16">All Events</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 1g:grid-cols-3 xl:grid-cols-4 gap-8"></div>
         {events.map((event) => (
-          <Card key={event.id} {...event} />
+          <Card
+            key={event.id}
+            {...event}
+            //imageUrl={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${event.imageUrl}`}
+          />
         ))}
       </div>
     </main>
--- components/card.tsx
+++ components/card.tsx
@@ -1,32 +1,37 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
   id: number;
-  name: string;
+  title: string;
   description: string;
-  imageUrl: string;
+  imageUrl: string; //string[] for supabase
 }
 
-const Card: React.FC<CardProps> = ({ id, name, description, imageUrl }) => {
+const Card: React.FC<CardProps> = ({ id, title, description, imageUrl }) => {
   return (
-    <div className="max-w-lg bg-white rounded-sm overflow-hidden shadow-lg h-full flex flex-col justify-between">
-      <div>
-        <div className="relative h-96 bg-center">
-          <Image
-            src={imageUrl}
-            layout="fill"
-            style={{ objectFit: "cover" }}
-            alt={name}
-            className="rounded-t"
-          />
-        </div>
-        <div className="px-6 py-2">
-          <div className="text-2xl mb-2 uppercase line-clamp-2">{name}</div>
-          <p className="text-gray-700 py-2 text-base truncate">{description}</p>
+    <Link href={`/events/${id}`}>
+      <div className="max-w-lg bg-white rounded-sm overflow-hidden shadow-lg h-full flex flex-col justify-between">
+        <div>
+          <div className="relative h-96 bg-center">
+            <Image
+              src={imageUrl} //imageUrl[0] for the supabase data. Just need the first image from the URL
+              layout="fill"
+              style={{ objectFit: "cover" }}
+              alt={title}
+              className="rounded-t"
+            />
+          </div>
+          <div className="px-6 py-2">
+            <div className="text-2xl mb-2 uppercase line-clamp-2">{title}</div>
+            <p className="text-gray-700 py-2 text-base truncate">
+              {description}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
