0a9d8b3c0f589d6d806c77d72f7beecb1e428a68
Sid <sidharth.singh0412@gmail.com>
2024-06-19 20:09:35 -0700

    finish postComponent

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,11 +1,92 @@
 import PostComponent from "@/components/postComponent";
+import { createClient } from "@/supabase/client";
+import { notFound } from "next/navigation";
 
-const Page = () => {
+interface EventData {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string[];
+}
+
+async function fetchEventData(slug: string): Promise<EventData | null> {
+  const supabase = createClient();
+  const { data: eventData, error } = await supabase
+    .from("cueup-events")
+    .select()
+    .match({ id: slug })
+    .single();
+
+  if (error) {
+    console.error("Error fetching event data:", error);
+    return null;
+  }
+
+  return eventData;
+}
+
+export async function generateStaticParams() {
+  const supabase = createClient();
+  const { data: events, error } = await supabase.from("cueup-events").select();
+
+  if (error) {
+    console.error("Error fetching event slugs:", error);
+    return [];
+  }
+
+  return events.map((event) => ({
+    slug: event.id,
+  }));
+}
+
+// export async function getStaticPaths() {
+//   const supabase = createClient();
+//   const { data: events, error } = await supabase.from("cueup-events").select();
+
+//   if (error) {
+//     console.error("Error fetching event slugs:", error);
+//     return { paths: [], fallback: "blocking" };
+//   }
+
+//   const paths = events.map((event) => ({
+//     params: { slug: event.id },
+//   }));
+
+//   return { paths, fallback: "blocking" };
+// }
+
+// export async function getStaticProps({ params }: { params: { slug: string } }) {
+//   const supabase = createClient();
+//   const { slug } = params;
+//   const { data: eventData, error } = await supabase
+//     .from("cueup-events")
+//     .select()
+//     .match({ id: slug })
+//     .single();
+
+//   if (error) {
+//     console.error("Error fetching event data:", error);
+//     return { notFound: true };
+//   }
+
+//   return {
+//     props: {
+//       eventData,
+//     },
+//     revalidate: 3600, // Revalidate the page at most once every hour
+//   };
+// }
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const eventData = await fetchEventData(slug);
+
+  if (!eventData) {
+    notFound();
+  }
   return (
     <div>
-      <PostComponent />
+      <PostComponent eventData={eventData} />
     </div>
   );
-};
-
-export default Page;
+}
