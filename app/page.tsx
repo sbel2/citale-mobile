03dfebfbb24f553aa90992de814795fb65200b81
Aditi <jha.aditi21@gmail.com>
2024-06-23 17:24:49 -0400

    home page formatting

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/supabase/client";
 import { cookies } from "next/headers";
 import Image from "next/legacy/image";
 
+
 export default async function Home() {
   const cookieStore = cookies();
   const supabase = createClient();
@@ -14,9 +15,9 @@ export default async function Home() {
   }
   return (
     <main className="min-h-screen mx-auto max-w-[100rem] overflow-x-hidden">
-      <div className="px-12 pt-12 pb-20">
+      <div className="px-12 pt-1 pb-20">
         <div className="flex flex-col xl:flex-row xl:gap-40">
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {events.map((event) => (
               <Card key={event.id} {...event} />
             ))}
--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Header({ font }: { font?: string }) {
   return (
-    <header className="py-2 bg-gray-952">
+    <header className="py-10 pb-4 bg-gray-952">
       <div className="max-w- [100rem] px-12 mx-auto flex justify-between">
         <Link href="/">
-          <h1 className={`uppercase text-yellow-500 text-centerpy-2 ${font}`}>
+          <h1 id="name" className={` uppercase text-3xl text-purple-500 text-center py-2 ${font}`}
+          style={{ color: '#57356a' }}>
             Cue Up
           </h1>
         </Link>
