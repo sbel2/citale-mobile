a02a7dddb399f8e624034ae8a8e26127e15e3be2
Yan Si <86997019+sbel2@users.noreply.github.com>
2024-07-25 15:28:19 -0400

    cleanup

--- components/card.tsx
+++ components/card.tsx
@@ -11,16 +11,17 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ post_id, title, imageUrl }) => {
   return (
-    <div className = {styles['main-container']}>
-    <Link href={`/events/${post_id}`} className={styles['link-wrapper']}>
+    
         <div className={styles['image-wrapper']}>
           <div className={styles['image-container']}>
+            <Link href={`/events/${post_id}`} className={styles['link-wrapper']}>
               <img
                 src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${imageUrl[0]}`}
                 alt={title}
                 className = 'transition-transform duration-500 ease-in-out transform'
               />
               <div className={styles['overlay']}></div>
+            </Link>
           </div>
           <div className='px-6 py-2'>
             <div className='text-1xl mb-2 line-clamp-3 text-black'>
@@ -28,8 +29,6 @@ const Card: React.FC<CardProps> = ({ post_id, title, imageUrl }) => {
             </div>{" "}
           </div>
         </div>
-    </Link>
-    </div>
   );
 };
 
