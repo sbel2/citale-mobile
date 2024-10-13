a656d0e735116645ddd28bf47781b94930de6642
Yan Si <86997019+sbel2@users.noreply.github.com>
2024-10-13 14:33:19 -0400

    changing suspense message

--- app/(explore)/search-results/page.tsx
+++ app/(explore)/search-results/page.tsx
@@ -11,7 +11,7 @@ import {handleSearch} from '../../lib/searchUtils'
 const MasonryGrid = dynamic(() => import('@/components/MasonryGrid'), { ssr: false });
 
 const Search = () => {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams(); 
   const query = searchParams.get('query');
   const [posts, setPosts] = useState<Post[]>([]);
   const [error, setError] = useState<string | null>(null);
@@ -67,7 +67,7 @@ const Search = () => {
 
 const SearchResult = () => {
   return (
-    <Suspense fallback={<div>Loading search parameters...</div>}>
+    <Suspense fallback={null}>
       <Search />
     </Suspense>
   );
