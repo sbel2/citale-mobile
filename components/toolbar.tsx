9b12f9606b91d27acdf7b7ce60cdc18215c471c2
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-01-05 21:40:51 -0500

    toolbar edit

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,13 +1,18 @@
 "use client"
 
 import React , {useEffect, useState} from "react";
-import { useAuth } from 'app/context/AuthContext';
 import { createClient } from '@/supabase/client';
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter, usePathname } from 'next/navigation';
 
 const Toolbar: React.FC = () => {
   const supabase = createClient()
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userId, setUserId] = useState<string | null>(null);
+  const { push } = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
     const getUser = async () => {
       const { data, error } = await supabase.auth.getUser();
@@ -52,40 +57,52 @@ const Toolbar: React.FC = () => {
   
 
   return (
-    <nav className="bg-gray-800 text-white fixed md:top-0 md:left-0 md:h-full md:w-64 w-full bottom-0 h-16 flex md:flex-col items-start md:items-stretch shadow-md z-50">
-      <a
-        href="#"
-        className="p-4 w-full text-center md:text-left hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-      >
-        Home
-      </a>
+    <nav className="bg-white text-black fixed md:top-0 md:left-0 md:h-full md:w-64 w-full bottom-0 h-16 flex md:flex-col items-start md:items-stretch shadow-md z-50"
+    style={{paddingBottom: 'env(safe-area-inset-bottom)'}}>
+     <Link href="/" aria-label="Home" className="pt-10 pl-8 pb-10 hidden md:inline">
+       <Image
+         src="/citale_header.svg"
+         alt="Citale Logo"
+         width={90}
+         height={30}
+         priority
+       />
+     </Link>
+     <button onClick={() => push('/')} className={`p-4 w-full flex justify-center md:justify-start items-center md:hover:bg-gray-200 focus:outline-none md:focus:ring-2 md:focus:ring-blue-500 transition-all`}>
+       <Image
+         src={pathname === '/' ? "/home_s.svg" : "/home.svg"}
+         alt="Home Icon"
+         width={25}
+         height={25}
+         priority
+       />
+       <span className="ml-5 hidden md:inline">Home</span>
+     </button>
+     <button onClick={() => push('/talebot')} className={`p-4 w-full flex justify-center md:justify-start items-center md:hover:bg-gray-200 focus:outline-none md:focus:ring-2 md:focus:ring-blue-500 transition-all`}>
+       <Image
+         src={pathname === '/talebot' ? "/robot_s.svg" : "/robot.svg"}
+         alt="Robot Icon"
+         width={25}
+         height={25}
+         priority
+       />
+       <span className="ml-5 hidden md:inline">Talebot</span>
+     </button>
       {isLoggedIn?(
         <a
         href="/account/profile"
-        className="p-4 w-full text-center md:text-left hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+        className="p-4 w-full flex justify-center md:justify-start items-center md:hover:bg-gray-200 focus:outline-none md:focus:ring-2 md:focus:ring-blue-500 transition-all"
         >
           Profile
         </a>
       ):(
         <a
         href="/log-in"
-        className="p-4 w-full text-center md:text-left hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+        className="p-4 w-full flex justify-center md:justify-start items-center md:hover:bg-gray-200 focus:outline-none md:focus:ring-2 md:focus:ring-blue-500 transition-all"
         >
           Profile
         </a>
       )}
-      <a
-        href="#"
-        className="p-4 w-full text-center md:text-left hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-      >
-        Swipe
-      </a>
-      <a
-        href="#"
-        className="p-4 w-full text-center md:text-left hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-      >
-        Settings
-      </a>
       {isLoggedIn ? (
         <button
           onClick={handleLogout}
