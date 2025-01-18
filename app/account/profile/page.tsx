aab04f94aa880a793858298b731448948fb09a80
annaandmandy <annaandtommy1004@gmail.com>
2025-01-17 21:38:09 -0800

    add display UserPosts Funtion

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import Linkify from 'react-linkify';
 import { Post } from '@/app/lib/types';
 import styles from '@/components/postComponent.module.css'
+import { Button } from '@nextui-org/react';
 
 const MasonryGrid = dynamic(() => import('@/components/MasonryGrid'), { ssr: false });
 
@@ -21,7 +22,9 @@ export default function ProfilePage() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setloading] = useState<boolean>(true);
     const [firstLoad, setFirstLoad] = useState<boolean>(true);
+    const [displayCAtagory, setDisplayCAtagory] = useState<string>('My Posts')
 
+    const postButtons = ['My Posts', 'My likes', 'My Archived'];
     // Fetch user profile data from Supabase
     useEffect(() => {
         if (user) {
@@ -39,37 +42,46 @@ export default function ProfilePage() {
                 setUserAvatar(data.avatar_url || 'avatar.png');  // Set avatar URL
                 setUserProfile(data);  // Store the entire user profile
                 setFetchSuccess(true);
+                handleFetchUserPosts(user.id);
             };
 
             fetchUserData();
-
-            const fetchPostData = async () => {
-                const { data, error } = await supabase
-                  .from("posts")
-                  .select("post_id, title, description, is_video, mediaUrl, mapUrl, thumbnailUrl, user_id, like_count, created_at")
-                  .eq('user_id', user.id);
-          
-                if (error || !data) {
-                  console.error('Error fetching post data:', error);
-                  setPosts([]);
-                  setloading(false);
-                  return;
-                } else {
-                  setPosts(data);
-                  setloading(false);
-                }
-              };
-
-        if(fetchSuccess) {
-            fetchPostData();
-        }
-
         } else {
             console.error('User is not logged in');
         }
     }, [user]);
 
-    
+    const handleFetchUserPosts = async (userId: string) => {
+        const { data, error } = await supabase
+        .from("posts")
+            .select("post_id, title, description, is_video, mediaUrl, mapUrl, thumbnailUrl, user_id, like_count, created_at")
+            .eq('user_id', userId);
+        if (error || !data) {
+            console.error('Error fetching post data:', error);
+            setPosts([]);
+            setloading(false);
+            } 
+        else {
+            setPosts(data);
+            setloading(false);
+        }
+    };
+
+    const handleFetchLikePosts = async (userId: string) => {
+    };
+
+    const handleArchivePosts = async (postId: string) => {
+    };
+
+    //check if user entered a query and calling onsearch to fetch results
+    const handleCategoryClick = async (option: string, userId: string) => {
+        setDisplayCAtagory(option);
+        if (option === 'My Posts'){
+            await handleFetchUserPosts(userId);
+            console.log(posts)
+        }
+    };
+
 
     // Link decorator for clickable URLs in bio
     const linkDecorator = (href: string, text: string, key: number): React.ReactNode => {
@@ -111,7 +123,7 @@ export default function ProfilePage() {
         <div className="flex flex-col items-center max-w-md mx-auto p-8 bg-gray-100 rounded-lg shadow-md">
             <div className="p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
                 {userProfile ? (
-                    <>
+                    <div>
                         <Image
                             className="h-32 w-32 rounded-full border-4 border-white mx-auto md:mx-0 mb-4"
                             src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/profile-pic/${userAvatar}`}
@@ -128,11 +140,30 @@ export default function ProfilePage() {
                             </div>
                         )}
                         <p className="text-sm text-gray-300">{userProfile.bio || 'No bio available'}</p>
-                    </>
+                    </div>
                 ) : (
                     <p>Loading...</p>
                 )}
             </div>
+            {user ? (
+                <div>
+                    <div className="flex m-2 xl:justify-center hide-scrollbar">
+                    {postButtons.map((category) => (
+                    <button
+                        key={category}
+                        type="button"
+                        onClick={() => handleCategoryClick(category, user.id)}
+                        className={`px-3 py-3 rounded-full text-sm min-w-max ${displayCAtagory === category || (displayCAtagory === 'myPosts' && category === 'myPosts') ? 'bg-gray-300' : 'bg-white'}`}
+                    >
+                    {category}
+                    </button>
+            ))}
+                </div>
+            </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+            
             <div className={styles.container}>
                 {posts.length === 0 ? (
                     <p className="text-center">No posts found :) </p>
