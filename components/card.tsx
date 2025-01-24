da5460c26223c63b3c79930b41500be50b168c62
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-01-23 22:23:34 -0500

    display user and heart on main feed

--- components/card.tsx
+++ components/card.tsx
@@ -8,10 +8,18 @@ import Image from "next/image";
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 import { Post } from "@/app/lib/types";
 import { useRouter } from "next/navigation";
+import { supabase } from "@/app/lib/definitions";
+import { useAuth } from 'app/context/AuthContext';
 
 const Card: React.FC<{ post: Post }> = ({ post }) => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(post.like_count);
+  const [username, setUsername] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('avatar.png');
+  const { user, logout } = useAuth();
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
 
   useEffect(() => {
     if (isOpen) {
@@ -35,6 +43,141 @@ const Card: React.FC<{ post: Post }> = ({ post }) => {
     router.back();
 };
 
+const handleLike = async () => {
+    if (!user) {
+      // Show login popup if the user is not authenticated
+      setShowLoginPopup(true);
+      return;
+    }
+  
+    try {
+      if (!liked) {
+        // Increment the like count in the 'likes' table
+        const { error: insertError } = await supabase
+          .from('likes')
+          .insert([{ user_id: user.id, post_id: post.post_id }]);
+  
+        if (insertError) {
+          console.error('Error adding like:', insertError.message);
+          return;
+        }
+  
+        // Increment the like count in the 'posts' table
+        const { error: updateError } = await supabase
+          .from('posts')
+          .update({ like_count: likesCount + 1 })
+          .eq('post_id', post.post_id);
+  
+        if (updateError) {
+          console.error('Error updating post like count:', updateError.message);
+          return;
+        }
+  
+        // Update state
+        setLikesCount((prev) => prev + 1);
+      } else {
+        // Remove the like from the 'likes' table
+        const { error: deleteError } = await supabase
+          .from('likes')
+          .delete()
+          .eq('user_id', user.id)
+          .eq('post_id', post.post_id);
+  
+        if (deleteError) {
+          console.error('Error removing like:', deleteError.message);
+          return;
+        }
+  
+        // Decrement the like count in the 'posts' table
+        const { error: updateError } = await supabase
+          .from('posts')
+          .update({ like_count: likesCount - 1 })
+          .eq('post_id', post.post_id);
+  
+        if (updateError) {
+          console.error('Error updating post like count:', updateError.message);
+          return;
+        }
+  
+        // Update state
+        setLikesCount((prev) => prev - 1);
+      }
+  
+      // Toggle the like state
+      setLiked(!liked);
+    } catch (error) {
+      console.error('Error handling like:', error);
+    }
+  };
+
+  useEffect(() => {
+      const handleFetchUserProfile = async () => {
+        // Fetch user profile data from the server
+        const {data, error} = await supabase
+          .from('profiles')
+          .select('username, avatar_url')
+          .eq('id', post.user_id)
+          .single();
+        if (error) {
+          console.error('Error fetching user profile:', error.message);
+          return;
+        }
+        if(data){
+          setUsername(data?.username || '');
+          setAvatarUrl(data?.avatar_url || '');
+          console.log(data);
+        }
+      };
+      handleFetchUserProfile();
+    }, [post.user_id]);
+  
+    useEffect(() => {
+      const fetchLikeStatus = async () => {
+        if (user) {
+          const { data, error } = await supabase
+            .from('likes')
+            .select('*')
+            .eq('user_id', user.id)
+            .eq('post_id', post.post_id)
+            .single();
+    
+          if (error) {
+            console.error('Error fetching like status:', error.message);
+            return;
+          }
+    
+          setLiked(!!data);
+        }
+      };
+    
+      fetchLikeStatus();
+    }, [user, post.post_id]);
+  
+    useEffect(() => {
+      const fetchUpdatedLikeCount = async () => {
+        try {
+          const { data, error } = await supabase
+            .from('posts')
+            .select('like_count')
+            .eq('post_id', post.post_id)
+            .single();
+            
+          if (error) {
+            console.error('Error fetching updated like count:', error.message);
+            return;
+          }
+    
+          if (data) {
+            setLikesCount(data.like_count); // Update the likesCount state with the latest value
+          }
+        } catch (err) {
+          console.error('Error fetching updated like count:', err);
+        }
+      };
+    
+      fetchUpdatedLikeCount();
+    }, [post.post_id]);  
+
   return (
     <Dialog open={isOpen} onOpenChange={(open) => {
       if (open !== isOpen) {
@@ -45,6 +188,7 @@ const Card: React.FC<{ post: Post }> = ({ post }) => {
       }
     }}>
       <DialogTrigger asChild>
+        <div>
         <div onClick={handleClick} className="cursor-pointer">
           <div className={styles['image-container']}>
             {post.is_video ? (
@@ -84,13 +228,15 @@ const Card: React.FC<{ post: Post }> = ({ post }) => {
             )}
             <div className={styles['overlay']}></div>
           </div>
-            <div className="px-2 py-3">
-              <div className="text-sm sm:text-base mb-1 2xl:mb-2 line-clamp-3 text-black">
+            <div className="px-2 pt-3">
+              <div className="text-xs sm:text-sm line-clamp-3 text-black">
                 {post.title}
               </div>
             </div>
         </div>
+      </div>
       </DialogTrigger>
+      
       <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
         <VisuallyHidden>
           <DialogTitle>{post.title}</DialogTitle>
@@ -105,8 +251,74 @@ const Card: React.FC<{ post: Post }> = ({ post }) => {
         >
         </DialogClose>
       </DialogContent>
+      <div className="flex items-center justify-between px-2 py-3">
+        {/* Profile section */}
+        <div className="flex items-center">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/profile-pic/${avatarUrl}`}
+            alt="Profile"
+            width={20}
+            height={20}
+            className="rounded-full"
+          />
+          <p className="text-xs ml-2 truncate max-w-[100px] text-gray-600">{username}</p>
+        </div>
+
+        {/* Like button */}
+        <button className="flex items-center p-1" onClick={handleLike}>
+          {liked ? (
+            <svg
+              fill="red"
+              stroke="red"
+              viewBox="0 0 24 24"
+              className="w-4 h-4 mr-1"
+            >
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+            </svg>
+          ) : (
+            <svg
+              fill="none"
+              stroke="black"
+              viewBox="0 0 24 24"
+              className="w-4 h-4 mr-1"
+            >
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+            </svg>
+          )}
+          <span className="text-xs">{likesCount}</span>
+        </button>
+        {/* Login popup */}
+        {showLoginPopup && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+            <div className="flex justify-center mb-3">
+              <Image src="/citale_header.svg" alt="Citale Logo" width={100} height={60} priority />
+            </div>
+              <p className="text-sm text-gray-600 mb-6">
+                We are so glad you like Citale! <br /><br />
+                Please sign in or sign up to interact with the community.
+              </p>
+              <div className="flex justify-center gap-6">
+                <button
+                  className="bg-[#fd0000] hover:bg-[#fd0000] text-white px-4 py-2 rounded mr-2"
+                  onClick={() => router.push('/log-in')}
+                >
+                  Log in
+                </button>
+                <button
+                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
+                  onClick={() => setShowLoginPopup(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </Dialog>
   );
 };
 
 export default Card;
+
