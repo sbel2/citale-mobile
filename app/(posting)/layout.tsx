73b58225a5efdad3c80a902191c6be4e2ad0b818
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-10 12:14:57 -0500

    code update hotjar script remove

--- app/(posting)/layout.tsx
+++ app/(posting)/layout.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header";
 import Toolbar from "@/components/toolbar"; 
 import { Inter } from "next/font/google";
 import '../globals.css';
@@ -8,7 +7,7 @@ import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Citale | Explore Boston",
+  title: "Citale | Posting",
   description: "Things to do in Boston",
 };
 
@@ -23,20 +22,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes = "any"/>
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        
-        {/* Hotjar Script */}
-        <Script id="hotjar-script" strategy="afterInteractive">
-          {`
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:5052807,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </Script>
       </head>
       <body className={`${inter.className} flex flex-col md:flex-row`}>
         {/* Toolbar on the left */}
