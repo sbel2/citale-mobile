556c9d76ad39efa5ec6eab853d23cdce5d63f3bf
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-12 17:21:21 -0500

    UI change

--- app/(posting)/layout.tsx
+++ app/(posting)/layout.tsx
@@ -1,40 +1,32 @@
-import Toolbar from "@/components/toolbar"; 
+import 'app/globals.css';
 import { Inter } from "next/font/google";
-import '../globals.css';
-import Script from 'next/script';
-import { Metadata } from "next";
+import Toolbar from "@/components/toolbar";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Citale | Posting",
-  description: "Things to do in Boston",
+export const metadata = {
+    title: 'Citale | Upload',
+    description: 'Upload Page',
 };
 
-export default function RootLayout({
-  children,
+export default function ProfileLayout({
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes = "any"/>
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-      </head>
-      <body className={`${inter.className} flex flex-col md:flex-row`}>
-        {/* Toolbar on the left */}
-        <Toolbar />
-
-        {/* Group Header and Main Content */}
-        <div className="flex-1 flex flex-col md:ml-64">
-          <main className="flex-1 p-4 bg-gray-951">
-            {children}
-          </main>
-        </div>
-      </body>
-
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} bg-white`}>
+                <div className="flex h-screen">
+                    <div className="md:w-64">
+                        <Toolbar />
+                    </div>
+                        <main className="flex-1 p-4 bg-gray-951">
+                            {children}
+                        </main>
+                </div>
+            </body>
+        </html>
+    );
 }
