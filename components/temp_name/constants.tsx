57a92d975eaa77227687881e2f3708193ec77134
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-13 23:29:36 -0500

    Temporarily renamed Share to temp_name

---
+++ components/temp_name/constants.tsx
@@ -0,0 +1,17 @@
+export const categoryList: string[] = [
+  'Outdoor', 'Sport', 'Photography', 'Shopping', 'Market',
+  'Music', 'Dating', 'Performance', 'Event', 'Museum', 'Food', 'Art'
+];
+
+export const locationList: string[] = [
+  'Allston', 'Back Bay', 'Beacon Hill', 'Brookline', 'Cambridge', 
+  'Downtown', 'Greater Boston', 'Somerville'
+];
+
+export const seasonList: string[] = [
+  'Spring', 'Summer', 'Fall', 'Winter'
+];
+
+export const priceList: string[] = [
+  'Free', '$', '$$', '$$$'
+];
