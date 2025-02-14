e2a69dccfe0d9b25e0aaa0fdf370aab69156c88c
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-14 14:27:00 -0500

    getting rid of posthog code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-import { AuthProvider } from 'app/context/AuthContext'; // Adjust the path to where your AuthProvider is located
-import Script from 'next/script';
-import posthog from 'posthog-js'
+import { AuthProvider } from 'app/context/AuthContext';
 
 export default function RootLayout({
   children,
@@ -13,18 +11,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes = "any"/>
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        
-        
-        {/* <Script strategy="afterInteractive">
-          {`
-            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-            posthog.init('phc_6Hzv7gC5fa3JF2qTdL6jJ62AXQJ4pIApNKWS8WY2CMF', {
-              api_host: 'https://us.i.posthog.com',
-              persistence: 'localStorage'
-            });
-          `}
-
-        </Script> */}
       </head>
       <body>
         <AuthProvider>
