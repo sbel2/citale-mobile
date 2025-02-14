c91b2109e55b4f635720931c9717aa6a77592796
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-14 14:42:45 -0500

    layout error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,15 @@
 import { AuthProvider } from 'app/context/AuthContext';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata = {
+  title: "Things to do in Boston",
+  description: "share and talk",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes = "any"/>
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-      </head>
-      <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+      <body className="bg-white">
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
