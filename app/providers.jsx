e525f0fe80e716914dce510cb07f48d68df0afc6
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-02-19 15:51:58 -0500

    env posthog image index fix

--- app/providers.jsx
+++ app/providers.jsx
@@ -10,8 +10,8 @@ export function PostHogProvider({ children }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-      capture_pageleave: true, // Enable pageleave capture
+      capture_pageview: false,
+      capture_pageleave: true,
     })
   }, [])
 
