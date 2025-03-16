9305e840f40868d8909b0bb177df61bc598ec989
sbel2 <86997019+sbel2@users.noreply.github.com>
2025-03-16 13:11:32 -0400

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.citaleco.app',
-  appName: 'Citale',
-  webDir: 'out',
+  appId: "com.citaleco.app",
+  appName: "Citale",
+  server: {
+    url: "https://citaleco.com", // ✅ Load directly from your live website
+    cleartext: true
+  }
 };
 
 export default config;
